15031305894e5c790274f456d95c2bb03d073a3c
alihanec <149117775+alihanec@users.noreply.github.com>
2023-10-27 01:30:48 +0300

    method

---
+++ Metotlar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            add();
+            Console.ReadLine();
+        }
+
+        static void add()
+        {
+            Console.WriteLine("Added");
+        }
+    }
+}
